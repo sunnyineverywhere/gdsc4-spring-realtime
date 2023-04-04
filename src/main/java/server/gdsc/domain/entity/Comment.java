df2b3446c04540e869d9d60b3129b2436951b22e
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-04-04 19:40:39 +0900

    ✨ feat: add Comment Entity #3

---
+++ src/main/java/server/gdsc/domain/entity/Comment.java
@@ -0,0 +1,38 @@
+package server.gdsc.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String commentId;
+
+    @NonNull
+    @Column()
+    private String contents;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public Comment(String commentId, @NonNull String contents, @NonNull Account account, @NonNull Post post) {
+        this.commentId = commentId;
+        this.contents = contents;
+        this.account = account;
+        this.post = post;
+    }
+}
